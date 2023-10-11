1fc56ff9cadb2c9f4653f062e0e173a61331102f
1234 <arpitsinghthakur321@gmail.com>
2023-10-12 00:07:18 +0530

    added popular product section

--- nike/src/sections/Hero.jsx
+++ nike/src/sections/Hero.jsx
@@ -53,10 +53,10 @@ const  [bigShoeImg , setBigShoeImg ] = useState(bigShoe1)
 
 
 
-      <div className="relative flex   justify-center items-center xl:min-h-screen max-xl:py-40  bg-primary bg-hero bg-cover bg-center  ">
+      <div className="relative flex    justify-center items-center xl:min-h-screen max-xl:py-40  bg-primary bg-hero bg-cover bg-center  ">
         <img src={bigShoeImg} alt="shoe collection" width={1200} height={500} className="object-contain relative z-10" />
 
-        <div className="flex sm:gap-6 gap-4 absolute -bottom-[-5%] sm:left-[10%] max-sm:px-6 ">
+        <div className="flex z-20 sm:gap-6 gap-4 absolute -bottom-[-5%] sm:left-[10%] max-sm:px-6 ">
           {shoes.map((shoe) =>
           (<div key={shoe} >
             <ShoeCard imgURL={shoe}
