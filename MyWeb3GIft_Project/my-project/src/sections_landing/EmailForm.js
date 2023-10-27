b175e1d1e69b53a85541a082862fc7d15b355867
1234 <arpitsinghthakur321@gmail.com>
2023-10-27 12:38:45 +0530

    test for checking deployement

--- MyWeb3GIft_Project/my-project/src/sections_landing/EmailForm.js
+++ MyWeb3GIft_Project/my-project/src/sections_landing/EmailForm.js
@@ -111,6 +111,7 @@ const EmailForm = () => {
                 </form>
                 {/* <div className="absolute inset-y-0 right-0 hidden lg:block lg:left-2/3 xl:left-1/2">
                     <picture>
+                    
                         <source srcSet="/images/object/5.webp" type="image/webp" />
                         <source srcSet="/images/object/5.png" />
                         <img className="object-cover w-1/2 mx-auto    maw-w-44" src="/images/object/5.png" alt="shopping item" />
